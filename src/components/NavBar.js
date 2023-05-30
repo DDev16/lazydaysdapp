af7f300810209683b6f324209b6be3b3e9db0cda
DDev16 <dillondevera12@gmail.com>
2023-05-30 12:26:48 -0700

    neworks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FiHome, FiPlusCircle, FiFolder } from 'react-icons/fi';
+import { Web3Context } from '../utils/Web3Provider.js';
+import songbirdLogo from '../assets/songbird-logo.png';
+import flareLogo from '../assets/flare-logo.png';
+
+
 // Styled Components
 const StyledNav = styled.nav`
   background-color: #252525;
@@ -67,7 +72,71 @@ const NavLinks = styled.ul`
   }
 `;
 
+const NetworkSelect = styled.select`
+  color: #ffffff;
+  background-color: #252525;
+  border: none;
+  margin-right: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 16px;
+  outline: none;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #333333;
+  }
+
+  &:focus {
+    background-color: #444444;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 5px;
+  vertical-align: middle;
+`;
+
 const NavBar = () => {
+  const { web3 } = useContext(Web3Context);
+  const [currentNetworkId, setCurrentNetworkId] = useState('');
+  const [selectedNetwork, setSelectedNetwork] = useState(null);
+
+  // update selected network on handleNetworkChange
+  const handleNetworkChange = async (e) => {
+    const networkId = parseInt(e.target.value, 10);
+    if (web3 && web3.currentProvider.isMetaMask) {
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: `0x${networkId.toString(16)}` }],
+        });
+        setSelectedNetwork(networkId === 19 ? songbirdLogo : flareLogo);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  useEffect(() => {
+    const getCurrentNetworkId = async () => {
+      if (web3 && web3.currentProvider.isMetaMask) {
+        try {
+          const networkId = await web3.eth.net.getId();
+          setCurrentNetworkId(networkId.toString());
+          setSelectedNetwork(networkId === 19 ? songbirdLogo : flareLogo);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+  
+    getCurrentNetworkId();
+  }, [web3]);
+  
+ 
+
   return (
     <StyledNav>
       <Logo>
@@ -95,12 +164,6 @@ const NavBar = () => {
             Batch Mint
           </Link>
         </li>
-        {/* <li>
-          <Link to="/deploy-contract">
-            <FiPlusCircle className="nav-icon" />
-            Deploy Contract
-          </Link>
-        </li> */}
         <li>
           <Link to="/my-tokens">
             <FiFolder className="nav-icon" />
@@ -108,6 +171,11 @@ const NavBar = () => {
           </Link>
         </li>
       </NavLinks>
+      <NetworkSelect value={currentNetworkId} onChange={handleNetworkChange}>
+  <option value="19">Songbird</option>
+  <option value="14">Flare</option>
+</NetworkSelect>
+{selectedNetwork && <LogoImage src={selectedNetwork} alt="Selected Network" />}
     </StyledNav>
   );
 };
--- src/utils/Web3Provider.js
+++ src/utils/Web3Provider.js
@@ -14,19 +14,31 @@ const Web3Provider = ({ children }) => {
         const web3Instance = new Web3(window.ethereum);
         await window.ethereum.enable();
         setWeb3(web3Instance);
+        setContract(null); // Reset contract when network changes
+
+        const handleNetworkChange = () => {
+          window.location.reload(); // Reload the page on network change
+        };
+
+        window.ethereum.on('chainChanged', handleNetworkChange);
+
+        const networkId = await web3Instance.eth.net.getId();
+        let contractAddress = '';
+
+        if (networkId === 19) {
+          // Songbird network
+          contractAddress = '0x9309a0A7229C0b039b7017872A96d544FC17DaDF';
+        } else if (networkId === 14) {
+          // Flare network
+          contractAddress = '0x92Dd5BF315b84F1fA0fB9865ca9130a45f99e117';
+        } else {
+          // Add more conditions for other networks
+          // ...
+        }
 
         const contractInstance = new web3Instance.eth.Contract(
           MyNFT.abi,
-          '0x9309a0A7229C0b039b7017872A96d544FC17DaDF'
-        );
-        setContract(contractInstance);
-      } else if (window.web3) {
-        const web3Instance = new Web3(window.web3);
-        setWeb3(web3Instance);
-
-        const contractInstance = new web3Instance.eth.Contract(
-          MyNFT.abi,
-          '0x9309a0A7229C0b039b7017872A96d544FC17DaDF'
+          contractAddress
         );
         setContract(contractInstance);
       } else {
